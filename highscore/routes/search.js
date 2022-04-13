1fbfc20563bd05a18d323add06c07caa0d4e9f62
Jacob Persson <jacobpersson96@gmail.com>
2022-04-13 16:42:57 +0200

    commit

--- highscore/routes/search.js
+++ highscore/routes/search.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Highscore" });
+  res.render("search", { title: "Sökresultat" });
 });
 
 module.exports = router;
