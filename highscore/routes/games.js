da43c2c29f9620f32e6c2d354519c869ebeaa4ca
Jacob Persson <jacobpersson96@gmail.com>
2022-05-03 11:01:47 +0200

    commit

--- highscore/routes/games.js
+++ highscore/routes/games.js
@@ -2,32 +2,35 @@ var express = require("express");
 var router = express.Router();
 
 // GET http://localhost:3000/
-router.get("/:url_slug", async function (req, res) {
+router.get("/:urlSlug", async function (req, res) {
   const db = req.app.locals.db;
 
   const sql = `
-  SELECT 
-    title,
-    genre,
-    release_date,
-    description,
-    url_slug
-    FROM game
-`;
-  const sql2 = `
   SELECT game,
-  player,
-  score_date,
-  points
-  FROM highscores
+    player,
+    score_date,
+    points
+    FROM highscores
 `;
+
+  const sql2 = `
+SELECT id,
+title,
+description,
+image_url,
+genre,
+release_date,
+url_slug
+FROM game
+`;
+
   const result = await db.query(sql);
   const result2 = await db.query(sql2);
 
   res.render("gameInfo", {
-    title: "Spelets title",
-    gamesInfo: result.row,
-    highscoreInfo: result2.row,
+    title: "highscore",
+    Highscores: result.rows,
+    Games: result2.rows,
   });
 });
 
